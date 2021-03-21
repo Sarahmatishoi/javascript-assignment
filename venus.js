dd2cf62b5f83e872bed31656d7d4cead9ab8669e
Sarahmatishoi <79209900+Sarahmatishoi@users.noreply.github.com>
2021-03-21 16:02:43 +0300

    Add files via upload

---
+++ venus.js
@@ -0,0 +1,23 @@
+var fruit={
+    name:"oranges",
+    price:30.00,
+    quantity:2,
+    fruitcost:function(price,quantity){
+        totalPrice=price*quantity
+        return this .fruitcost=totalPrice
+
+    }
+
+
+
+}
+fruit.fruitcost(30.00,2);
+console.log ( fruit.quantity + " " + fruit.name + " "  + " for  KES " + totalPrice );
+
+/*examples of objects-bank account,pen.
+examples of methods-toUpperCase(),indexOf()
+examples of a function-function square(),function greet()
+*\
+
+
+
